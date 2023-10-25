c670dd319971f94bfb762092cbfb6f4ca9a10745
Reuben Ramos <rsramos23@gmail.com>
2023-10-25 10:17:23 -0700

    removed uneeded code and added a button in html

--- js/script.js
+++ js/script.js
@@ -2,12 +2,7 @@ const empForm = document.getElementById('empForm')
 
 empForm.addEventListener("submit", (e) => {
     e.preventDefault()
-    // parseFloat(name.value),
-    // parseFloat(id.value),
-    // parseFloat(ext.value)
-    // parseFloat(email.value)
-    // parseFloat(department.value)
-    // parseFloat(submit.value)
+
 const name = document.getElementById("id").value
 console.log(`8-Digit Employee ID: ${name}`)
 const id = document.getElementById("name").value
@@ -18,8 +13,7 @@ const email = document.getElementById("email").value
 console.log(`E-mail ${email}`)
 const department = document.getElementById("department").value
 console.log(`Department: ${department}`)
-const submit = document.getElementById("submit").value
+// const submit = document.getElementById("submit").value
 })
-// empForm.innerHTML = id.value + ext.value
 
 
