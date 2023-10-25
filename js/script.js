b5b9a3f20c07b4f561ac19da2f6034648c66b61e
Reuben Ramos <rsramos23@gmail.com>
2023-10-25 10:10:10 -0700

    completed javascript form code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-const ems () => {
+const empForm = document.getElementById('empForm')
 
-}
+empForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    // parseFloat(name.value),
+    // parseFloat(id.value),
+    // parseFloat(ext.value)
+    // parseFloat(email.value)
+    // parseFloat(department.value)
+    // parseFloat(submit.value)
+const name = document.getElementById("id").value
+console.log(`8-Digit Employee ID: ${name}`)
+const id = document.getElementById("name").value
+console.log(`Full Name: ${id}`)
+const ext = document.getElementById("ext").value
+console.log(`4-Digit Extension: ${ext}`)
+const email = document.getElementById("email").value
+console.log(`E-mail ${email}`)
+const department = document.getElementById("department").value
+console.log(`Department: ${department}`)
+const submit = document.getElementById("submit").value
+})
+// empForm.innerHTML = id.value + ext.value
+
+
